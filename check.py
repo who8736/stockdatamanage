31fc5794a439ccccf6d1782ada4b7d5c46552c9b
who8736 <who8736@hotmail.com>
2020-04-21 17:20:49 +0800

    分类器试验

--- check.py
+++ check.py
@@ -1,8 +1,9 @@
-# from sqlrw
+import numpy as np
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
 from sqlconn import engine
+from sqlrw import readStockList
 
 
 def checkGuben():
@@ -59,3 +60,58 @@ def checkQuarterData(ts_code):
         return 4, div
     else:
         return 0, div
+
+def checkQuarterDatanew():
+    """
+    按每日指标和利润表分别计算TTM利润，
+    两者差异超过0.00001时表示需更新财务季报
+    利润表应取最后一季，最后一季上年同期，上年年末计算得到profits_ttm
+    :return:
+    无利润表 返回1
+    无每日指标 返回2
+    利润表数据不全 返回3
+    每日指标与利润表差异较大 返回4
+    无差异 返回0
+    """
+    stocks = readStockList()
+    sql = f"""
+            select ts_code, pb, total_mv, circ_mv from daily_basic
+            where trade_date=
+                (select max(trade_date) from daily_basic)
+            """
+    # result = np.array(engine.execute(sql).fetchall()).reshape(-1)
+    df = pd.read_sql(sql, engine)
+
+
+
+    # pbdf = df.dropna()
+    # # nopbdf = df[df.ts_code not in pbdf]
+    # nopbdf = df[df.pb.isna() | df.total_mv.isna()]
+    # # pbdf
+    # # print(result)
+    # print(df.head())
+    # print(pbdf.head())
+    # print(nopbdf)
+    # print(len(df), len(pbdf), len(nopbdf))
+
+    sql = f"""
+            select a.ts_code, a.total_hldr_eqy_exc_min_int eqy,
+                    total_hldr_eqy_inc_min_int eqy1
+            from balancesheet a,
+                (select ts_code, max(end_date) edate 
+                    from balancesheet group by ts_code) b
+            where a.ts_code=b.ts_code and a.end_date=b.edate
+            """
+    qydf = pd.read_sql(sql, engine)
+    df = pd.merge(df, qydf, how='left', left_on='ts_code', right_on='ts_code')
+    print(df)
+    print(df[df.isnull().T.any()])
+    noqydf = df[df.isnull().T.any()]
+    qydf = df.dropna()
+    print(len(df), len(qydf), len(noqydf))
+    qydf['cha'] = qydf.total_mv * 10000 / qydf.eqy - qydf.pb
+
+
+if __name__ == '__main__':
+    pass
+    checkQuarterDatanew()
